516dbe584a4f19278f2420e6cc9797450f440819
smokra <ivan.smokrovic@gmail.com>
2020-12-03 14:19:25 +0100

    Adding new page animation with load page delay

--- script.js
+++ script.js
@@ -1,31 +1,16 @@
-// wait until DOM is ready
-document.addEventListener("DOMContentLoaded", function(event) {
-  
-  // wait until window is loaded - all images, styles-sheets, fonts, links, and other media assets
-  // you could also use addEventListener() instead
-  window.onload = function() {
-    
-     // OPTIONAL - waits til next tick render to run code (prevents running in the middle of render tick)
-     window.requestAnimationFrame(function() {
-    
-        // GSAP custom code goes here     
-        let tl = gsap.timeline()
-        tl.from(".logo", {duration: 1.5, rotation: -180, opacity: 0, scale: 0.2, ease:"bounce"})
-          
-     });
-    
-  };
+var tl = gsap.timeline({ defaults:{opacity:0, ease:"back"}});
+gsap.set(".page-wrapper", {rotationY:15})
+function init() {
+    tl.from(".page-wrapper", {autoAlpha:0})  //this nullifies visibility:none; from CSS 
+    .from(".logo", {duration: 1.5, rotation: -180, scale: 0.2, ease:"bounce"})
+    .from("h1, h3", {x:80, duration:1}, "-=1")
+    .from("h2", {x:-80, duration:1}, "<")
+    .from(".animate", {y:30}, "-=0.2")
+    .from(".img-box", {scale:0, transformOrigin:"50% 50%", stagger:0.2}, "-=0.5")
+    .from(".form-group", {scale:0, transformOrigin:"50% 50%", stagger:0.2}, "-=0.5")
+    .from("button", {y:50}, "-=0.1") 
+}
 
+window.addEventListener("load", function(event) { 
+  init(); //do stuff after page load it self, don't forget about autoAlpha
 });
-
-
- /* .to(".car.yellow", {duration: 5, x:500, ease:"none"}, "-=1")
-  //this allows the green car to come in as the yellow car leaves
-  .fromTo(".car.green", {x:0} , {duration: 5, x:500, ease:"none"}, "-=1")
-
-//this creates the seamless loop from time of 1 second to 9 seconds
-let controller = tl.tweenFromTo(1, 9, {ease:"none", repeat:-1})
-
-//click anywhere to toggle play/pause
-document.addEventListener("click", () => controller.paused(!controller.paused()))
-*/
