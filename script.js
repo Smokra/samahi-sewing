b9028469e787633a4fc708cf0c81f256f4cc3f71
smokra <ivan.smokrovic@gmail.com>
2020-12-30 11:45:27 +0100

    Finishing page animation and fixing responsivness

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ var tl = gsap.timeline({ defaults:{opacity:0, ease:"back"}});
 gsap.set(".page-wrapper", {rotationY:15})
 function init() {
     tl.from(".page-wrapper", {autoAlpha:0})  //this nullifies visibility:hidden; from CSS 
+    //.to(".content-bg", {opacity:1, scale:1, transformOrigin:"50% 50%", duration:0.9})
     .from(".logo", {duration: 1.5, rotation: -180, scale: 0.2, ease:"bounce"})
     .from("h1, h3", {x:80, duration:1}, "-=1")
     .from("h2", {x:-80, duration:1}, "<")
     .from("p", {y:80, duration:1}, "-=0.2")
     .from(".animate", {y:30}, "-=0.2")
     .from(".img-box", {scale:0, transformOrigin:"50% 50%", stagger:0.2}, "-=0.5")
-    .from(".form-group", {scale:0, transformOrigin:"50% 50%", stagger:0.2}, "-=0.5")
+    .from(".form-group", {scale:0, transformOrigin:"50% 50%", stagger:0.2}, "-=1")
     .from("button", {y:50}, "-=0.1") 
 }
 
